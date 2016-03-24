0b4a0269ff39a632d9eca3aeebccaa22bf4a175e
Graham Findlay <graham.b.findlay@gmail.com>
2016-03-23 21:45:34 -0500

    removed driver code from oizumi2014_fig4

--- graphiit/example_graphs/oizumi2014_fig4.py
+++ graphiit/example_graphs/oizumi2014_fig4.py
@@ -6,12 +6,3 @@ graph_conf = [('A', OR, 'B', 'C'),
             ('C', XOR, 'A', 'B')]
 
 state1 = {'on': ['A']}
-
-# TODO : move this to separate file
-if __name__ == "__main__":
-    graph = g.Graph(graph_conf, state1)
-    concepts = graph.net_first_order_concepts(just_phi=False, use_background_nodes=False)
-    print(concepts)
-    big_phi = graph.net_first_order_mip()
-    print(big_phi)
-    import pdb; pdb.set_trace()
